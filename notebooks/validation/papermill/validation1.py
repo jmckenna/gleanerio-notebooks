d15144f3bb4776265611abad20872a090a2daf71
Douglas Fils <drfils@gmail.com>
2022-08-19 20:59:54 -0500

    Moslty valiation updates

--- notebooks/validation/papermill/validation1.py
+++ notebooks/validation/papermill/validation1.py
@@ -4,4 +4,4 @@ pm.execute_notebook(
         '../shacl_simple.ipynb',
         './output.ipynb',
         parameters = dict(dg="https://raw.githubusercontent.com/geoschemas-org/geoshapes/master/datagraphs/dataset-full-BAD.json-ld",
-            sgiurl="https://raw.githubusercontent.com/ESIPFed/science-on-schema.org/master/validation/shapegraphs/soso_common_v1.2.3.ttl"))
+            sgurl="https://raw.githubusercontent.com/ESIPFed/science-on-schema.org/master/validation/shapegraphs/soso_common_v1.2.3.ttl"))
